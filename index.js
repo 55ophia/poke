3c906ee8eb72b52db1706b7612e9edfb527365fe
katiek27 <katiek5412@gmail.com>
2023-10-12 11:33:28 -0700

    adding character stuff, CHANGE COORDS -kt

--- index.js
+++ index.js
@@ -10,9 +10,22 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 
 const image = new Image()
 image.src = "./images/bg.png"
-console.log(image)
 
+//i wonder if we can take these sprites and change this code up?? //
+const playerImage = new Image()
+playerImage.src = 'x'
 
 image.onload = () => {
-    c.drawImage(image, -2100, -900)
+    c.drawImage(image, -2100, -900) // you might need to change this
+    c.drawImage(
+        playerImage,
+        0,
+        0,
+        playerImage.width / 4,
+        playerImage.height,
+        canvas.width / 2 - (playerImage.width / 4) / 2, //take a look at these too pls
+        canvas.height / 2 - playerImage.height / 2,
+        playerImage.width / 4,
+        playerImage.height,
+    )
 }
