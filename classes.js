ceb94d4e406d05b64902903b11fbca585b0d7258
katiek27 <katiek5412@gmail.com>
2023-10-20 11:58:59 -0700

    a little bit of collision - kt

--- classes.js
+++ classes.js
@@ -49,7 +49,7 @@ if(this.frames.elapsed % 10 === 0) {
 
 
     draw() {
-        c.fillStyle = "rgba(255, 0, 0, 0.0"
+        c.fillStyle = 'rgba(255, 0, 0, 0.5)'
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
 }
