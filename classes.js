2049d124a964a0ac37f5d34ed270bcb533fa8d43
andri-c <andrewchang2906@gmail.com>
2023-10-31 18:17:49 +0000

    fixed my problem and let the pokemon faint -andrew

--- classes.js
+++ classes.js
@@ -113,7 +113,7 @@ class Monster extends Sprite {
 
   faint() {
     document.querySelector('#dialogueBox'). innerHTML = this.name + ' fainted!'
-    gsap.to(thisposition, {
+    gsap.to(this.position, {
       y: this.position. y + 20
     })
       gsap.to(this, {
